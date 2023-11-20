ab2f9ecb278a73f45e1d843e819e9f4a0c596dfa
jihoonahn <jihoonahn.dev@gmail.com>
2023-11-20 20:06:00 +0900

    Add Environment Object

---
+++ ProjectDescriptionHelpers/Environment/EnvironmentObject.swift
@@ -0,0 +1,10 @@
+import ProjectDescription
+
+/// EnvironmentObject Type
+/// Helps manage environmental variables that are used repeatedly by Tuist.
+///
+/// ```swift
+/// final class AppEnvironment: EnvironmentObject {}
+/// ```
+///
+public protocol EnvironmentObject: AnyObject {}
