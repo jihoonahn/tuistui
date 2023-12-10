7b63eccddd786add060b97f4ee22ba052bd0395b
jihoonahn <jihoonahn.dev@gmail.com>
2023-12-10 23:06:28 +0900

    Update Template

--- Templates/Project/project.swift
+++ Templates/Project/project.swift
@@ -4,15 +4,15 @@ private let pathAttribute = Template.Attribute.optional("path", default: "Projec
 private let nameAttribute = Template.Attribute.required("name")
 
 private let template = Template(
-    description: "A template for a new workspace module",
+    description: "Project module template",
     attributes: [
         pathAttribute,
         nameAttribute
     ],
     items: [
         .file(
-            path: "\(pathAttribute)/\(nameAttribute)/Workspace.swift",
-            templatePath: "Workspace.stencil"
+            path: "\(pathAttribute)/\(nameAttribute)/Project.swift",
+            templatePath: "Project.stencil"
         )
     ]
 )
--- Templates/Workspace/workspace.swift
+++ Templates/Workspace/workspace.swift
@@ -4,15 +4,15 @@ private let pathAttribute = Template.Attribute.optional("path", default: "Projec
 private let nameAttribute = Template.Attribute.required("name")
 
 private let template = Template(
-    description: "A template for a new project module",
+    description: "Workspace module template",
     attributes: [
         pathAttribute,
         nameAttribute
     ],
     items: [
         .file(
-            path: "\(pathAttribute)/\(nameAttribute)/Project.swift",
-            templatePath: "Project.stencil"
+            path: "\(pathAttribute)/\(nameAttribute)/Workspace.swift",
+            templatePath: "Workspace.stencil"
         )
     ]
 )
