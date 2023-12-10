bf66d6c0aa7b8a4165aea528c16ec6d3108f0b8b
jihoonahn <jihoonahn.dev@gmail.com>
2023-12-10 23:30:12 +0900

    Add Template

---
+++ Templates/project/project.swift
@@ -0,0 +1,18 @@
+import ProjectDescription
+
+private let pathAttribute = Template.Attribute.optional("path", default: "Projects")
+private let nameAttribute = Template.Attribute.required("name")
+
+private let template = Template(
+    description: "Project module template",
+    attributes: [
+        pathAttribute,
+        nameAttribute
+    ],
+    items: [
+        .file(
+            path: "\(pathAttribute)/\(nameAttribute)/Project.swift",
+            templatePath: "Project.stencil"
+        )
+    ]
+)
