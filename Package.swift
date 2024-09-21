fe4f3f9a9b9245174dd1b3b101062e42fee1a0fe
jihoonahn.dev <jihoonahn.dev@gmail.com>
2024-09-21 04:12:10 +0000

    Revert Tuist Version

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
-        .package(url: "https://github.com/tuist/tuist", from: "4.27.0"),
+        .package(url: "https://github.com/tuist/tuist", from: "4.15.0"),
     ],
     targets: [
         .executableTarget(name: "TuistUICLI"),
